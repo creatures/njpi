1e202e3063c5df8418c7d8541cb9e44ab53d87bc
Neil Heinrich <nheinrich@gmail.com>
2015-10-21 00:04:06 -0600

    add build optimizations

--- config.rb
+++ config.rb
@@ -1,15 +1,14 @@
-###
+# ------------------------------------------------------------------------------
 # Compass
-###
 
 # Change Compass configuration
 # compass_config do |config|
 #   config.output_style = :compact
 # end
 
-###
-# Page options, layouts, aliases and proxies
-###
+
+# ------------------------------------------------------------------------------
+# Layouts
 
 # Per-page layout changes:
 #
@@ -24,49 +23,78 @@
 #   page "/admin/*"
 # end
 
+
+# ------------------------------------------------------------------------------
+# Proxy
+
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-###
-# Helpers
-###
+
+# ------------------------------------------------------------------------------
+# Images
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+# ------------------------------------------------------------------------------
+# Paths
 
 set :css_dir, "stylesheets"
-
 set :js_dir, "javascripts"
-
 set :images_dir, "images"
 
-# Build-specific configuration
+
+# ------------------------------------------------------------------------------
+# URLs
+
+# Pretty URLs - https://middlemanapp.com/advanced/pretty_urls/
+
+activate :directory_indexes
+
+
+# ------------------------------------------------------------------------------
+# Development
+
+configure :development do
+
+  # LiveReload
+  activate :livereload
+
+end
+
+
+# ------------------------------------------------------------------------------
+# Build
+
 configure :build do
-  # For example, change the Compass output style for deployment
+
+  # Minify CSS
   activate :minify_css
 
-  # Minify Javascript on build
+  # Minify HTML
+  activate :minify_html
+
+  # Minify Javascript
   activate :minify_javascript
+
+  # Optimize Images
+  activate :imageoptim do |options|
+    options.pngout = false
+  end
 
   # Enable cache buster
   activate :asset_hash
+
+  # gzip
+  activate :gzip
 
   # Use relative URLs
   # activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
 end
