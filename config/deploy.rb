7a10fa07eced6bfd224c29fd550d0e5a17ae3348
Neil Heinrich <nheinrich@gmail.com>
2015-10-21 22:08:43 -0600

    set up capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+# ------------------------------------------------------------------------------
+# Configuration
+
+lock "3.4.0"
+
+set :application, "creatures.io"
+set :stage, "production"
+set :use_sudo, false
+set :build_dir, "build"
+set :ssh_login, "deploy@app.pixels-and-bits.com"
+set :deploy_dir, "/ebs/sites/#{fetch(:application)}/current"
+
+
+# ------------------------------------------------------------------------------
+# Deploy
+
+namespace :deploy do
+
+  desc "Build and deploy middleman project"
+  task :middleman do
+    invoke 'middleman:build'
+    invoke 'middleman:deploy'
+  end
+
+end
+
+# ------------------------------------------------------------------------------
+# Middleman
+
+namespace :middleman do
+
+  desc "Run build utility"
+  task :build do
+    run_locally do
+     execute "middleman build"
+    end
+  end
+
+  desc "Push"
+  task :deploy do
+    run_locally do
+     execute "scp -r #{fetch(:build_dir)}/* #{fetch(:ssh_login)}:#{fetch(:deploy_dir)}"
+    end
+  end
+
+end
